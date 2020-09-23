db935c3cc3f6a9ae3a7acf4f712ddb7802daf37f
Marie-Helene Burle <marie.burle@westgrid.ca>
2020-09-23 16:04:44 -0700

    important commit: remove all the intermediate step from 3rd nbody script now that they are all in a function

--- static/src/winterschool2020/test2.jl
+++ static/src/winterschool2020/test2.jl
@@ -14,46 +14,6 @@ bodies = [SLArray{Tuple{5}}(id=i::Int,
                             v=rand(Float64, 3),
                             a=rand(Float64, 3)) for i in 1:n]
 
-# * Acceleration
-
-acc = []
-for k in 1:3
-    tmp0 = []
-    for i in 1:n
-        tmp1 = []
-        for j in filter(!=(i), 1:n)
-            push!(tmp1, (G * bodies[j].m * (bodies[i].r[k] - bodies[j].r[k]) /
-                         abs(sqrt((bodies[i].r[1] - bodies[j].r[1])^2 +
-                                  (bodies[i].r[2] - bodies[j].r[2])^2 +
-                                  (bodies[i].r[3] - bodies[j].r[3])^2))^3))
-        end
-        push!(tmp0, sum(tmp1, dims=1)[1])
-    end
-    push!(acc, tmp0)
-end
-
-acc = reshape(collect(Iterators.flatten(acc)), n, 3)
-
-# * Velocity
-
-# v = v + a dt
-
-for k in 1:3
-    for i in 1:n
-        bodies[i].v[k] += acc[i, k] * dt
-    end
-end
-
-# * Position
-
-# r = r + v dt
-
-for k in 1:3
-    for i in 1:n
-        bodies[i].r[k] += bodies[i].v[k] * dt
-    end
-end
-
 # * Step
 
 function sim!()
